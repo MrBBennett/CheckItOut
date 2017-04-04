85159a320cd40a605757537ec541b27b6813d3c1
MrBBennett <brynbennett@hotmail.com>
2017-04-04 10:38:56 +0100

    Uploaded files for VB.Net solution to Check It Out

---
+++ CheckItOut/LylCrdCheck.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLylCrdCheck
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblCustomerName = New System.Windows.Forms.Label()
+        Me.txtCustomerName = New System.Windows.Forms.TextBox()
+        Me.lblExpiryDate = New System.Windows.Forms.Label()
+        Me.txtCardNumber = New System.Windows.Forms.TextBox()
+        Me.lblCardNumber = New System.Windows.Forms.Label()
+        Me.dtpExpiry = New System.Windows.Forms.DateTimePicker()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.txtPostcode = New System.Windows.Forms.TextBox()
+        Me.lblPostcode = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblCustomerName
+        '
+        Me.lblCustomerName.AutoSize = True
+        Me.lblCustomerName.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCustomerName.Location = New System.Drawing.Point(37, 23)
+        Me.lblCustomerName.Name = "lblCustomerName"
+        Me.lblCustomerName.Size = New System.Drawing.Size(166, 24)
+        Me.lblCustomerName.TabIndex = 0
+        Me.lblCustomerName.Text = "Customer Name:"
+        '
+        'txtCustomerName
+        '
+        Me.txtCustomerName.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCustomerName.Location = New System.Drawing.Point(40, 58)
+        Me.txtCustomerName.Name = "txtCustomerName"
+        Me.txtCustomerName.Size = New System.Drawing.Size(273, 29)
+        Me.txtCustomerName.TabIndex = 1
+        '
+        'lblExpiryDate
+        '
+        Me.lblExpiryDate.AutoSize = True
+        Me.lblExpiryDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblExpiryDate.Location = New System.Drawing.Point(37, 97)
+        Me.lblExpiryDate.Name = "lblExpiryDate"
+        Me.lblExpiryDate.Size = New System.Drawing.Size(123, 24)
+        Me.lblExpiryDate.TabIndex = 2
+        Me.lblExpiryDate.Text = "Expiry Date:"
+        '
+        'txtCardNumber
+        '
+        Me.txtCardNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCardNumber.Location = New System.Drawing.Point(341, 129)
+        Me.txtCardNumber.Name = "txtCardNumber"
+        Me.txtCardNumber.Size = New System.Drawing.Size(280, 29)
+        Me.txtCardNumber.TabIndex = 4
+        '
+        'lblCardNumber
+        '
+        Me.lblCardNumber.AutoSize = True
+        Me.lblCardNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCardNumber.Location = New System.Drawing.Point(338, 94)
+        Me.lblCardNumber.Name = "lblCardNumber"
+        Me.lblCardNumber.Size = New System.Drawing.Size(211, 24)
+        Me.lblCardNumber.TabIndex = 4
+        Me.lblCardNumber.Text = "Loyalty Card Number:"
+        '
+        'dtpExpiry
+        '
+        Me.dtpExpiry.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpExpiry.Location = New System.Drawing.Point(41, 129)
+        Me.dtpExpiry.Name = "dtpExpiry"
+        Me.dtpExpiry.Size = New System.Drawing.Size(272, 29)
+        Me.dtpExpiry.TabIndex = 3
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCheck.Location = New System.Drawing.Point(179, 205)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(134, 33)
+        Me.btnCheck.TabIndex = 5
+        Me.btnCheck.Text = "Check"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(345, 205)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(134, 33)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'txtPostcode
+        '
+        Me.txtPostcode.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPostcode.Location = New System.Drawing.Point(344, 58)
+        Me.txtPostcode.Name = "txtPostcode"
+        Me.txtPostcode.Size = New System.Drawing.Size(273, 29)
+        Me.txtPostcode.TabIndex = 2
+        '
+        'lblPostcode
+        '
+        Me.lblPostcode.AutoSize = True
+        Me.lblPostcode.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPostcode.Location = New System.Drawing.Point(341, 23)
+        Me.lblPostcode.Name = "lblPostcode"
+        Me.lblPostcode.Size = New System.Drawing.Size(198, 24)
+        Me.lblPostcode.TabIndex = 9
+        Me.lblPostcode.Text = "Customer Postcode:"
+        '
+        'frmLylCrdCheck
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Honeydew
+        Me.ClientSize = New System.Drawing.Size(665, 268)
+        Me.Controls.Add(Me.txtPostcode)
+        Me.Controls.Add(Me.lblPostcode)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.dtpExpiry)
+        Me.Controls.Add(Me.txtCardNumber)
+        Me.Controls.Add(Me.lblCardNumber)
+        Me.Controls.Add(Me.lblExpiryDate)
+        Me.Controls.Add(Me.txtCustomerName)
+        Me.Controls.Add(Me.lblCustomerName)
+        Me.Name = "frmLylCrdCheck"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Sharon's Jewellery Loyalty Card Checker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblCustomerName As System.Windows.Forms.Label
+    Friend WithEvents txtCustomerName As System.Windows.Forms.TextBox
+    Friend WithEvents lblExpiryDate As System.Windows.Forms.Label
+    Friend WithEvents txtCardNumber As System.Windows.Forms.TextBox
+    Friend WithEvents lblCardNumber As System.Windows.Forms.Label
+    Friend WithEvents dtpExpiry As System.Windows.Forms.DateTimePicker
+    Friend WithEvents btnCheck As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents txtPostcode As System.Windows.Forms.TextBox
+    Friend WithEvents lblPostcode As System.Windows.Forms.Label
+
+End Class
---
+++ CheckItOut/LylCrdCheck.vb
@@ -0,0 +1,73 @@
+﻿Public Class frmLylCrdCheck
+
+    Private Sub btnCheck_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCheck.Click
+        Dim CardNumber As String = txtCardNumber.Text
+        Dim ANumber As UShort = 0
+        Dim Total As UShort = 0
+        Dim CustomerName As String = txtCustomerName.Text
+
+        CardNumber = CardNumber.Replace(" ", "")
+
+        If CustomerName = "" Then
+            MsgBox("Please enter the customer name", MsgBoxStyle.OkOnly, "Error!")
+            txtCustomerName.Focus()
+            txtCustomerName.BackColor = Color.Yellow
+        ElseIf txtPostcode.Text = "" Then
+            MsgBox("Please enter the customer's postcode", MsgBoxStyle.OkOnly, "Error!")
+            txtPostcode.Focus()
+            txtPostcode.BackColor = Color.Yellow
+        ElseIf (CardNumber = "") Or (IsNumeric(CardNumber) = False) Or (Len(CardNumber) <> 8) Then
+            MsgBox("Please enter a valid card number", MsgBoxStyle.OkOnly, "Error!")
+            txtCardNumber.Focus()
+            txtCardNumber.BackColor = Color.Yellow
+        ElseIf dtpExpiry.Value < Today.Date Then
+            MsgBox("Sorry " & CustomerName & "  your card has expired", MsgBoxStyle.OkOnly, "Expired Card!")
+        Else
+            CardNumber = Mid(CardNumber, 1, 7)
+            CardNumber = StrReverse(CardNumber) & "0"
+            For i = 0 To 6 Step 2
+                ANumber = Val(CardNumber(i))
+                ANumber = ANumber * 2
+                If ANumber > 9 Then
+                    ANumber = ANumber - 9
+                End If
+                Total = Total + ANumber + Val(CardNumber(i + 1))
+            Next
+            Total = Total + Val(txtCardNumber.Text(7))
+            If Total Mod 10 = 0 Then
+                MsgBox(CustomerName & " your loyalty card is valid. Enjoy your 10% discount!", MsgBoxStyle.OkOnly, "Valid Card")
+            Else
+                MsgBox("Sorry " & CustomerName & " that loyalty card number is invalid", MsgBoxStyle.OkOnly, "Invalid Card")
+                txtCardNumber.Focus()
+                txtCardNumber.BackColor = Color.Yellow
+            End If
+
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        txtCardNumber.Text = ""
+        txtCustomerName.Text = ""
+        txtPostcode.Text = ""
+        dtpExpiry.Value = Today.Date
+        txtCardNumber.BackColor = Color.White
+        txtCustomerName.BackColor = Color.White
+        txtPostcode.BackColor = Color.White
+    End Sub
+
+    Private Sub txtCustomerName_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtCustomerName.TextChanged
+        txtCustomerName.BackColor = Color.White
+    End Sub
+
+    Private Sub txtCardNumber_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtCardNumber.TextChanged
+        txtCardNumber.BackColor = Color.White
+    End Sub
+
+    Private Sub frmLylCrdCheck_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        dtpExpiry.Value = DateAdd(DateInterval.Day, -1, dtpExpiry.Value)
+    End Sub
+
+    Private Sub txtPostcode_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtPostcode.TextChanged
+        txtPostcode.BackColor = Color.White
+    End Sub
+End Class
